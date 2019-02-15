ea8b834041f0f216050d92df8eb1b07847744798
mariusukr <marius@menucard.dk>
2019-02-15 12:58:41 +0100

    Update app.js
    HOTFIX: Update Mlab

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ var routes = require("./routes/index");
 var users = require("./routes/users");
 
 var configDB = require("./config/database.js");
-mongoose.connect(configDB.url);
-//mongoose.connect('mongodb://USERNAME:PASS@ds151523.mlab.com:51523/heroku_6jkcdgb5')
+mongoose.connect(mongodb://mariusukr:A-P10project-@ds151523.mlab.com:51523/heroku_6jkcdgb5);
 
 var cors = require('cors')
 
